7d5d8b8a7c20850f4be15573b5fb0e44e326dbf4
Weey <piaoxuewuyin@gmail.com>
2022-02-16 23:48:48 +0800

    将ng-zorro的配置从主模块抽出

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NZ_I18N } from 'ng-zorro-antd/i18n';
-import { zh_CN } from 'ng-zorro-antd/i18n';
-import { registerLocaleData } from '@angular/common';
-import zh from '@angular/common/locales/zh';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from "./shared/shared.module";
 import { CoreModule } from "./core/core.module";
-
-registerLocaleData(zh);
 
 @NgModule({
   declarations: [
@@ -28,7 +22,7 @@ registerLocaleData(zh);
     CoreModule,
     SharedModule
   ],
-  providers: [{ provide: NZ_I18N, useValue: zh_CN }],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,25 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule, Optional, SkipSelf} from '@angular/core';
+import {CommonModule, registerLocaleData} from '@angular/common';
+import zh from "@angular/common/locales/zh";
+import {NZ_I18N, zh_CN} from "ng-zorro-antd/i18n";
 
-
+// 设置成中文环境
+registerLocaleData(zh);
 
 @NgModule({
   declarations: [],
+  providers: [
+    // 引入antd相关配置
+    { provide: NZ_I18N, useValue: zh_CN }
+  ],
   imports: [
     CommonModule
   ]
 })
-export class CoreModule { }
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    if (parentModule) {
+      throw new Error(`CoreModule has already been loaded. Import Core modules in the AppModule only.`);
+    }
+  }
+}
