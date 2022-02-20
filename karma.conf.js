3923486246b605662f08557813182516bb342e98
Weey <piaoxuewuyin@gmail.com>
2022-02-20 23:34:15 +0800

    更改项目名

--- karma.conf.js
+++ karma.conf.js
@@ -25,12 +25,9 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/gofiber-admin'),
+      dir: require('path').join(__dirname, './coverage/fiber-admin'),
       subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
-      ]
+      reporters: [{ type: 'html' }, { type: 'text-summary' }]
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
@@ -40,5 +37,5 @@ module.exports = function (config) {
     browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true
-  });
-};
+  })
+}
